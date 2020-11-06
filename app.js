f0377bee9e0b2fe9fffeb47d5a0fef4610403180
Michał Gacek <gacek.michael@outlook.com>
2020-11-06 16:47:32 +0100

    Added subscription

--- app.js
+++ app.js
@@ -1,4 +1,4 @@
-const { GraphQLServer } = require("graphql-yoga");
+const { GraphQLServer, PubSub } = require("graphql-yoga");
 const posts = require('./data.json');
 
 const PORT = 4000;
@@ -15,6 +15,15 @@ const typeDefs = `
         title: String
         body: String
     }
+    type Mutation {
+        addPost(userId: Int, title: String!, body: String!): Post
+    }
+    type Subscription {
+        counter: Counter
+    }
+    type Counter {
+        count: Int!
+    }
 `;
 
 const resolvers = {
@@ -22,9 +31,33 @@ const resolvers = {
         hello: (_, { name }) => `Hello ${name || 'World'}`,
         allPosts: () => posts,
         post: (_, { postId }) => posts.filter(post => post.id === postId)[0]
+    },
+    Mutation: {
+        addPost: (_, { userId, title, body }) => {
+            // adding post
+
+            return {
+                id: 999,
+                userId,
+                title,
+                body
+            };
+        }
+    },
+    Subscription: {
+        counter: {
+            subscribe: (parent, args, { pubsub }) => {
+                let count = 0;
+                
+                setInterval(() => pubsub.publish("test", { counter: { count: count++ } }), 2000);
+
+                return pubsub.asyncIterator("test");
+            }
+        }
     }
 }
 
-const server = new GraphQLServer({ typeDefs, resolvers });
+const pubsub = new PubSub();
+const server = new GraphQLServer({ typeDefs, resolvers, context: { pubsub } });
 
 server.start(() => console.log(`Server is running on localhost:${PORT}`));
