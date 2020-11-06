314fd9108f8b4b183d7b7f00cfcc88ba953ccf75
Michał Gacek <gacek.michael@outlook.com>
2020-11-06 16:18:42 +0100

    Added posts with query

--- app.js
+++ app.js
@@ -1,15 +1,27 @@
 const { GraphQLServer } = require("graphql-yoga");
+const posts = require('./data.json');
+
 const PORT = 4000;
 
 const typeDefs = `
     type Query {
         hello(name: String): String!
+        post(postId: Int!): Post
+        allPosts: [Post!]!
+    }
+    type Post {
+        userId: Int
+        id: ID,
+        title: String
+        body: String
     }
 `;
 
 const resolvers = {
     Query: {
         hello: (_, { name }) => `Hello ${name || 'World'}`,
+        allPosts: () => posts,
+        post: (_, { postId }) => posts.filter(post => post.id === postId)[0]
     }
 }
 
